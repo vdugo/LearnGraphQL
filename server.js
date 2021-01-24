cf78ae436b6ca57086a628c35a0a1b92305d328e
vdugo <v3rdugo@yahoo.com>
2021-01-24 13:54:51 -0800

    Added author data and created schema.

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
 const express = require('express');
 
+const {graphqlHTTP} = require('express-graphql');
+
+const {GraphQLSchema, GraphQLObjectType, GraphQLString} = require('graphql');
+
 const app = express();
 
+// create the schema to query from
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: "HelloWorld",
+        fields: () => ({
+            message: { 
+                type: GraphQLString,
+                resolve: () => "Hello World"
+            }
+        })
+    })
+});
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}));
 app.listen(5000., () => console.log('Server running'));
